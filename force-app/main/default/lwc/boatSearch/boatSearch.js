b6acee96006baa4074df71edf58916485008a823
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-03-12 23:03:56 +0300

    add spiner in boatSeartch and methods in JS file

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+// imports
+import { LightningElement } from "lwc";
 
-export default class BoatSearch extends LightningElement {}
+export default class BoatSearch extends LightningElement {
+    isLoading = false;
+
+    // Handles loading event
+    handleLoading() {
+        this.isLoading = true;
+    }
+
+    // Handles done loading event
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) {}
+
+    createNewBoat() {}
+}
